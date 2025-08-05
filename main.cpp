7e3600e77bbaa7fd7ece1706a5b3458f991d2852
Alessia Barborini <alessia.barborini@studio.unibo.it>
2025-08-05 18:46:48 +0200

    acquisizione pattern

--- main.cpp
+++ main.cpp
@@ -2,19 +2,80 @@
 #include <iostream>
 
 int main() {
+  struct Pixel {
+    double pr;
+    double pg;
+    double pb;
+  };
+
   sf::Image image1;
   sf::Image image2;
   sf::Texture texture1;
   sf::Texture texture2;
 
-  sf::Vector2u pillars{image1.getSize()};
-  sf::Vector2u deep{image2.getSize()};
+  auto pillars{image1.getSize()};
+  auto deep{image2.getSize()};
 
-  double bx{pillars.x / deep.x};
-  double by{pillars.y / deep.y};
+  auto bx{pillars.x / deep.x};  // forse il tipo va specificato
+  auto by{pillars.y / deep.y};
 
-  for (int x = 0; x < pillars.x; x++) {
+ std::vector<Pixel> p;
+
+  for (unsigned int r = 0; r < pillars.x; r++) {
+    double x{static_cast<double>(r) / static_cast<double>(bx)};
+    unsigned int i{static_cast<unsigned int>(x)};
+    double s{x - i};
+
+    for (unsigned int c = 0; c < pillars.y; c++) {
+      double y{static_cast<double>(c) / static_cast<double>(by)};
+      unsigned int j{static_cast<unsigned int>(y)};
+      double t{y - j};
+
+      sf::Color p1 = image2.getPixel(i, j);
+      sf::Color p2 = image2.getPixel(i + 1, j);
+      sf::Color p3 = image2.getPixel(i, j + 1);
+      sf::Color p4 = image2.getPixel(i + 1, j + 1);
+
+      double pr = (1 - s) * (1 - t) * p1.r + s * (1 - t) * p2.r +
+                  (1 - s) * t * p3.r + s * t * p4.r;
+      double pg = (1 - s) * (1 - t) * p1.g + s * (1 - t) * p2.g +
+                  (1 - s) * t * p3.g + s * t * p4.g;
+      double pb = (1 - s) * (1 - t) * p1.b + s * (1 - t) * p2.b +
+                  (1 - s) * t * p3.b + s * t * p4.b;
+
+      p.push_back({pr, pg, pb});
+    }
   }
+
+  std::vector<int> pattern1;
+  std::vector<int> pattern2;
+
+  for (unsigned int r = 0; r < pillars.x; r++) {
+    for (unsigned int c = 0; c < pillars.y; c++) {
+      sf::Color pix = image1.getPixel(r, c);
+      double m{(pix.r + pix.g + pix.b) / 3.};
+      if (m < 127) {
+        pattern1.push_back(-1);
+      } else {
+        pattern1.push_back(+1);
+      }
+    }
+  }
+
+  for (unsigned int r = 0; r < deep.x; r++) {
+    for (unsigned int c = 0; c < deep.y; c++) {
+      double m{(p.pr + p.pg + p.pb) / 3.};
+      if (m < 127) {
+        pattern2.push_back(-1);
+      } else {
+        pattern2.push_back(+1);
+      }
+    }
+  }
+
+  // auto [width, height] = texture2.getSize();
+  // std::vector<uint8_t> pixels(width * height * 3);  // preso da sfml
+  // texture2.update(pixels.data());
 
   sf::RenderWindow window(sf::VideoMode(800, 600), "Neural Network");
 
